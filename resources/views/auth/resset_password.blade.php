9db1f9dfba8f1d45653f1431e6ec92c8c36167cf
Kareimovich <you@example.com>
2020-12-25 05:15:13 +0200

    blade

---
+++ resources/views/auth/resset_password.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.main')
+@php
+    $menu = false ;
+@endphp
+
+@section('content')
+
+
+
+<div class="overlay-s"></div>
+        <div class="jumpo col-xs-12" style="background-image: url({{ asset('/web/images/cover.jpg')}})">
+            <div class="container">
+                <h3>@lang('general.login')</h3>
+                <ul>
+                    <li>
+                        <a href="#">@lang('general.home')</a>
+                    </li>
+                    <li>@lang('general.login')</li>
+                </ul>
+            </div>
+        </div>
+        <main class="main-content col-xs-12">
+            <div class="sign-wrap col-xs-12">
+                <div class="container">
+                    <form action="{{route('do_login')}}" method="post">
+                        @csrf
+                        <div class="form-group">
+                        <input type="text" name="email" required placeholder="@lang('general.email')" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" name="password" required placeholder="@lang('general.password')" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label>
+                                <input type="checkbox">
+                                <span>@lang('general.remember_me')</span>
+                            </label>
+                            <a href="#">@lang('general.forget_password')</a>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn">@lang('login')</button>
+                            <p>@lang('general.register_text') <a href="#">@lang('general.register')</a></p>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </main>
+
+@endsection
+
+
+
