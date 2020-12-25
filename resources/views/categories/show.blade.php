a6ced5f653141bc5c0d5646ea6aee686fc39c31c
Kareimovich <you@example.com>
2020-12-26 01:33:21 +0200

    blade

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -277,7 +277,7 @@ $menu = false ;
                    
                  
                 @foreach($products as $product)
-                    <div class="block b-product col-md-2 col-sm-6 col-xs-12">
+                    <div class="block b-product col-md-3 col-sm-6 col-xs-12">
                         <div class="inner">
                             <div class="i-img">
                                 @if($product->product_condition == "new")
