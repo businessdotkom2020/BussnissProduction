406e0e307ebcd38beb9df6cf94f94b24c5a902f3
Kareimovich <you@example.com>
2020-12-25 16:29:36 +0200

    blade

--- app/Http/Controllers/web/HomeController.php
+++ app/Http/Controllers/web/HomeController.php
@@ -8,7 +8,18 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function home(){
-        return view('home');
-
+        $latest_products = \App\Models\Product::latest()->limit(12)->get();
+        $common_products = \App\Models\Product::limit(12)->orderBy('name','asc')->get();
+        $latest_requests = \App\Models\Request::latest()->limit(12)->get();
+        $home_suppliers = \App\Models\User::where('type','supplier')->get();
+        $home_services = \App\Models\Service::latest()->limit(12)->get();
+        
+        return view('home',compact(
+            'latest_products',
+            'common_products',
+            'latest_requests',
+            'home_suppliers',
+            'home_services',
+        ));
     }
 }
