f986775ca7330cc693f2b8201c1e139f7a1e8fab
Kareimovich <you@example.com>
2020-12-25 16:34:57 +0200

    blade

--- app/Http/Controllers/web/HomeController.php
+++ app/Http/Controllers/web/HomeController.php
@@ -9,11 +9,14 @@ class HomeController extends Controller
 {
     public function home(){
         $latest_products = \App\Models\Product::latest()->limit(12)->get();
+        return $latest_products_ids = $latest_products->pluck('id');
         $common_products = \App\Models\Product::limit(12)->orderBy('name','asc')->get();
         $latest_requests = \App\Models\Request::latest()->limit(12)->get();
         $home_suppliers = \App\Models\User::where('type','supplier')->get();
         $home_services = \App\Models\Service::latest()->limit(12)->get();
-        
+
+
+
         return view('home',compact(
             'latest_products',
             'common_products',
