cf70e65cf8e26a9fd1879908c8689de93bbbeac4
Kareimovich <you@example.com>
2020-12-25 16:41:21 +0200

    blade

--- app/Http/Controllers/web/HomeController.php
+++ app/Http/Controllers/web/HomeController.php
@@ -10,13 +10,23 @@ class HomeController extends Controller
     public function home(){
         $latest_products = \App\Models\Product::latest()->limit(12)->get();
         $latest_products_ids = $latest_products->pluck('id');
+        
         $common_products = \App\Models\Product::limit(12)->orderBy('name','asc')->get();
+        $common_products_ids = $common_products->pluck('id');
+        
         $latest_requests = \App\Models\Request::latest()->limit(12)->get();
+        $latest_requests_ids = $latest_requests->pluck('id');
+        
         $home_suppliers = \App\Models\User::where('type','supplier')->get();
+ 
         $home_services = \App\Models\Service::latest()->limit(12)->get();
+        $home_services_id = $home_services->pluck('id');
 
+  
+    $result = array_merge($home_services_id, $latest_requests_ids ,$common_products_ids ,$latest_products_ids);
 
-
+    return $result;
+    
         return view('home',compact(
             'latest_products',
             'common_products',
