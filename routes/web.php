a4ac36a65df1aeded3f81b34978373aeeccfeee9
Kareimovich <you@example.com>
2020-12-25 02:49:11 +0200

    blade

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
-if (App::environment('production')) {
+// if (App::environment('production')) {
     URL::forceScheme('https');
-}
+// }
 
 /*
 |--------------------------------------------------------------------------
