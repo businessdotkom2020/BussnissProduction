fbf9739787d36928fb509d56ee55b2bda6f5d443
= <=>
2021-01-18 16:46:56 +0200

    $m

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -127,22 +127,73 @@ $menu = true ;
             @foreach ($latest_requests as $product)
             <div class="slide-item">
                 <div class="block b-product">
-                    <div class="inner">
-                        <div class="i-img">
-                            <a href="javascript:void(0)" id="fav-req-{{$product->id}}" title="add to favourite" data-placement="top" class="fav-req-{{$product->id}} {{$product->isFavorited() ? 'fav-active' : null  }} fav-pro " onclick="favtoggle({{$product->id }},{{Auth::user() ? Auth::user()->id : null}})">
-                                <i class="fa fa-heart"></i>
-                            </a>
-                            <a href="{{url('product/'.$product->id)}}" class="img-hold">
-                                <img src="{{ json_decode($product->images ) ? url('storage/'.(json_decode($product->images))[0]) : url('storage/products/default.jpg') }}">
-                                <img src="{{  isset((json_decode($product->images))[1]) ? url('storage/'.(json_decode($product->images))[1]) : url('storage/products/default.jpg') }}" class="sec-img">
-                            </a>
-                        </div>
-                        <div class="i-data">
-                            <a href="{{url('product/'.$product->id)}}" class="title">{{$product->getTranslatedAttribute('name',\App::getLocale())}}</a>
-                            <p>{{ Str::limit($product->getTranslatedAttribute('description',\App::getLocale()),50 )}}</p>
-                            <a class="btn" href="#" data-toggle="modal" data-target="#contact_{{$product->user_id}}" target="_blank">@lang('general.contact_supplier')</a>
-                        </div>
-                    </div>
+                <div class="inner">
+                                        <div class="i-img">
+                                            @if($product->product_condition == 'new')
+                                            <div class="ribbon">
+                                                <span>@lang('general.new')</span>
+                                            </div>
+                                            @endif
+
+                                            <div class="prod-extra" style="position: inherit">
+
+                                                <a href="javascript:void(0)" id="fav-{{$product->id}}" title="add to favourite" data-placement="top" class="fav-{{$product->id}} {{$product->isFavorited() ? 'fav-active' : null  }} fav-pro " onclick="favtoggle({{$product->id }},{{Auth::user() ? Auth::user()->id : null}})">
+                                                    <i class="fa fa-heart"></i>
+                                                </a>
+                                            </div>
+
+
+
+                                            <a href="{{url('product/'.$product->id)}}" class="img-hold">
+                                                <img src="{{url('storage/'.$product->image)}}" alt="">
+                                                <img src="{{ json_decode($product->images ) ? url('storage/'.(json_decode($product->images))[0]) : "https://i.imgur.com/mFI2maG.jpg" }}" class="sec-img">
+
+                                            </a>
+                                        </div>
+                                        <div class="i-data">
+
+                                            <a href="{{url('product/'.$product->id)}}" class="title">{{$product->getTranslatedAttribute('name',\App::getLocale())}}</a>
+
+
+
+                                            <!-- <p>{{ Str::limit($product->getTranslatedAttribute('description',\App::getLocale()),50 )}}</p> -->
+
+                                            <div class="cardo" style="flex-grow: 1;padding:0px">
+                                                <div class="c-inner" style="text-align: right;">
+                                                    <div class="c-data">
+                                                        <p>
+                                                            @php $rating = $product->average_rating ; @endphp
+                                                            @foreach(range(1,5) as $i)
+                                                            @if($rating >0)
+                                                            @if($rating > 0.5)
+                                                            <i class="fa fa-star active"></i>
+                                                            @elseif($rating < 0.5 && $rating> 0)
+                                                                <i class="fas fa-star-half"></i>
+                                                                @endif
+                                                                @else
+                                                                <i class="fa fa-star"></i>
+                                                                @endif
+                                                                @php $rating--; @endphp
+
+                                                                @endforeach
+
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="block-price">
+                                            <span class="new-price">{{$product->sale_price ? $product->sale_price : $product->price}}$</span>  
+                                            <!-- Price before offer --> 
+                                            <span class="old-price">120$</span>
+                                            @if($product->sale_price)
+                                            <span class="offer-p">{{number_format( (($product->sale_price/$product->price) * 100) ,2 ) }} % تخفيض</span>
+                                            @endif
+                                            </div>
+                                            <a class="btn" href="#" data-toggle="modal" data-target="#contact_{{$product->user_id}}" target="_blank">@lang('general.contact_supplier')</a>
+                                        </div>
+
+                                    </div>
                 </div>
             </div>
             @endforeach
