6b8a4ff190b0162f671d53911b4306435b24b8c8
businessdotkom2020 <businessdotkom2020@gmail.com>
2021-01-20 16:20:14 +0200

    s

--- resources/views/dashboard/products.blade.php
+++ resources/views/dashboard/products.blade.php
@@ -6,90 +6,94 @@ $menu = false ;
 
 
 @section('content')
- <div class="overlay-s"></div>
-        <div class="jumpo col-xs-12" style="background-image: url(http://businessdotkom.com/web/images/cover.jpg)">
-            <div class="container">
-                <h3>Products</h3>
-                <ul>
-                    <li>
-                        <a href="{{url('/')}}">الرئيسية</a>
-                    </li>
-                    <li>Products</li>
-                </ul>
+<div class="overlay-s"></div>
+<div class="jumpo col-xs-12" style="background-image: url({{url('/web/images/cover.jpg')}} )">
+    <div class="container">
+        <h3>Products</h3>
+        <ul>
+            <li>
+                <a href="{{url('/')}}">الرئيسية</a>
+            </li>
+            <li>Products</li>
+        </ul>
+    </div>
+</div>
+
+
+
+<main class="main-content col-xs-12">
+
+    <div class="container">
+
+
+        <div class="form-group col-md-12 col-xs-12" style="text-align: center;margin-top: 15px;">
+            <button onclick="window.location='{{ url('products/new') }}'" type="submit" class="btn">Add Product <i
+                    class="fa fa-plus"></i></button>
+        </div>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        @foreach(\App\Models\Product::where('user_id',\Auth::id())->get() as $product)
+        <div class="block b-product col-md-3 col-sm-6 col-xs-12">
+            <div class="inner">
+                <div class="i-img">
+                    <a href="javascript:void(0)" id="fav-{{$product->id}}"
+                        class="fav-{{$product->id}} fav-pro {{$product->isFavorited() ? 'fav-active' : null  }}"
+                        onclick="favtoggle({{$product->id }},{{Auth::user() ? Auth::user()->id : null}})">
+                        <i class="fa fa-heart"></i>
+                    </a>
+
+                    <a href="{{url('product/'.$product->id.'/edit')}}" style="left: auto;right: 10px;" class="fav-pro">
+                        <i class="fa fa-edit"></i>
+                    </a>
+                    <a href="{{url('product/'.$product->id.'/delete')}}"
+                        style="left: auto;right: 10px;margin-top: 50px;" class="fav-pro">
+                        <i class="fa fa-trash"></i>
+                    </a>
+
+                    <a href="{{url('product/'.$product->id)}}" class="img-hold">
+
+                        <img src="{{ url('storage/'.$product->image)}}" alt="">
+                        @if(isset(json_decode($product->images)[0]))
+                        <img src="{{ url('storage/'.json_decode($product->images)[0])}}" class="sec-img" alt="">
+                        @endif
+
+                    </a>
+                </div>
+                <div class="i-data">
+                    <a href="{{url('product/'.$product->id)}}"
+                        class="title">{{$product->getTranslatedAttribute('name',\App::getLocale())}}</a>
+                    <p>{{str_limit($product->getTranslatedAttribute('description',\App::getLocale()), 110)}}</p>
+
+
+                    @if($product->sale_price)
+                    <span>{{$product->sale_price}} l.e</span>
+                    @endif
+
+
+                    <span class="{{$product->sale_price ? 'old' : ''}}">{{$product->sale_price}} l.e</span>
+                </div>
             </div>
         </div>
-        
-  
-        
-                <main class="main-content col-xs-12">
+        @endforeach
 
-                <div class="container">
-         
-         
-                      <div class="form-group col-md-12 col-xs-12" style="text-align: center;margin-top: 15px;">
-                            <button  onclick="window.location='{{ url('products/new') }}'"  type="submit" class="btn">Add Product <i class="fa fa-plus"></i></button>
-                        </div>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-                       @foreach(\App\Models\Product::where('user_id',\Auth::id())->get() as $product)
-                            <div class="block b-product col-md-3 col-sm-6 col-xs-12">
-                        <div class="inner">
-                            <div class="i-img">
-              <a href="javascript:void(0)" id="fav-{{$product->id}}" class="fav-{{$product->id}} fav-pro {{$product->isFavorited() ? 'fav-active' : null  }}"  onclick="favtoggle({{$product->id }},{{Auth::user() ? Auth::user()->id : null}})">
-                                <i class="fa fa-heart"></i>
-                            </a>
-                                
-                                <a href="{{url('product/'.$product->id.'/edit')}}" style="left: auto;right: 10px;" class="fav-pro">
-                                    <i class="fa fa-edit"></i>
-                                </a>
-                                <a href="{{url('product/'.$product->id.'/delete')}}" style="left: auto;right: 10px;margin-top: 50px;" class="fav-pro">
-                                    <i class="fa fa-trash"></i>
-                                </a>
-                                
-                                <a href="{{url('product/'.$product->id)}}" class="img-hold">
 
-                                    <img src="{{ url('storage/'.$product->image)}}" alt="">
-                                @if(isset(json_decode($product->images)[0]))
-                                    <img src="{{ url('storage/'.json_decode($product->images)[0])}}" class="sec-img" alt="">
-                                @endif
+    </div>
 
-                                </a>
-                            </div>
-                            <div class="i-data">
-                                <a href="{{url('product/'.$product->id)}}" class="title">{{$product->getTranslatedAttribute('name',\App::getLocale())}}</a>
-                                <p>{{str_limit($product->getTranslatedAttribute('description',\App::getLocale()), 110)}}</p>
-                              
-                              
-                                @if($product->sale_price)
-                                <span>{{$product->sale_price}} l.e</span>
-                                @endif
-          
-
-                                <span class="{{$product->sale_price ? 'old' : ''}}">{{$product->sale_price}} l.e</span>
-                            </div>
-                        </div>
-                    </div>
-                           @endforeach
-                    
-
-            </div>
-
-        </main>
+</main>
 
 @endsection
 @push('scripts')
 
 <script>
-
-     function favtoggle(product_id, user_id){
+    function favtoggle(product_id, user_id){
 
              var token = '{{ Session::token() }}';
 
 
-        
+
          $.ajax({
 
 
@@ -148,4 +152,3 @@ Toast.fire({
 </script>
 
 @endpush
-
