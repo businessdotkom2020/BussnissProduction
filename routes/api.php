a8f5c93cca541f7d41e41f90b595a8bb678c161e
businessdotkom2020 <businessdotkom2020@gmail.com>
2021-01-21 15:41:59 +0200

    s

--- routes/api.php
+++ routes/api.php
@@ -19,13 +19,32 @@ Route::get('supplier/{supplier_id}/sub-sub-categories/{sub_sub_category_id}', 'a
 
 Route::post('search', 'api\HomeController@filter');
 Route::get('curency', function ($amount = 44, $from = 'usd', $to = 'egp') {
-    $from    = 'JPY';
-    $to    = 'USD';
-    $amount  = 1;
-    $data = file_get_contents("http://google.com/ig/calculator?a=$amount&from=$from&to=$to");
-    preg_match("/<span class=bld>(.*)<\/span>/", $data, $converted);
-    $converted = preg_replace("/[^0-9.]/", "", $converted[1][0]);
-    return number_format(round($converted, 3), 2);
+    $url = "http://www.google.com/finance/converter?a=$amount&from=$from&to=$to";
+
+    $request = curl_init();
+    $timeOut = 0;
+    curl_setopt ($request, CURLOPT_URL, $url);
+    curl_setopt ($request, CURLOPT_RETURNTRANSFER, 1);
+
+    curl_setopt ($request, CURLOPT_USERAGENT,"Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1)");
+    curl_setopt ($request, CURLOPT_CONNECTTIMEOUT, $timeOut);
+    $response = curl_exec($request);
+    curl_close($request);
+
+    return $response;
+   }
+
+   if(isset($_POST['convert_currency']))
+   {
+    $amount=$_POST['amount'];
+    $from=$_POST['convert_from'];
+    $to=$_POST['convert_to'];
+
+    $rawData = currency_converter($from,$to,$amount);
+    $regex = '#\<span class=bld\>(.+?)\<\/span\>#s';
+    preg_match($regex, $rawData, $converted);
+    $result = $converted[0];
+    echo $result;
 });
 
 
