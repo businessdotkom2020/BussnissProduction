f841fdc6d159fff15b90e05628ad19b8376fee8e
= <=>
2021-02-04 17:01:07 +0200

    $m

--- public/web/js/script.js
+++ public/web/js/script.js
@@ -170,6 +170,49 @@ $(document).ready(function () {
     $('select').niceSelect();
 
 
+    // ------------step-wizard-------------
+    $('.nav-tabs > li a[title]').tooltip();
+    
+    //Wizard
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+
+        var target = $(e.target);
+    
+        if (target.parent().hasClass('disabled')) {
+            return false;
+        }
+    });
+
+    $(".next-step").click(function (e) {
+
+        var active = $('.wizard .nav-tabs li.active');
+        active.next().removeClass('disabled');
+        nextTab(active);
+
+    });
+    $(".prev-step").click(function (e) {
+
+        var active = $('.wizard .nav-tabs li.active');
+        prevTab(active);
+
+    });
+
+    function nextTab(elem) {
+        $(elem).next().find('a[data-toggle="tab"]').click();
+    }
+    function prevTab(elem) {
+        $(elem).prev().find('a[data-toggle="tab"]').click();
+    }
+
+
+    $('.nav-tabs').on('click', 'li', function() {
+        $('.nav-tabs li.active').removeClass('active');
+        $(this).addClass('active');
+    });
+
+
+
+
     var changeSlide = 4; // mobile -1, desktop + 1
     // Resize and refresh page. slider-two slideBy bug remove
     var slide = changeSlide;
