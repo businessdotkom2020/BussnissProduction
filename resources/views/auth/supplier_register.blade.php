551ac5cbd3001f617089fb87f08d59aee7c99434
= <=>
2021-02-05 16:43:43 +0200

    $m

--- resources/views/auth/supplier_register.blade.php
+++ resources/views/auth/supplier_register.blade.php
@@ -338,52 +338,45 @@ $menu = false ;
 
 $(document).ready(function () {
 
-var navListItems = $('div.setup-panel div a'),
-        allWells = $('.setup-content'),
-        allNextBtn = $('.nextBtn');
+    var navListItems = $('div.setup-panel div a'),
+            allWells = $('.setup-content'),
+            allNextBtn = $('.nextBtn');
 
-allWells.hide();
+    allWells.hide();
 
-navListItems.click(function (e) {
-    e.preventDefault();
-    var $target = $($(this).attr('href')),
-            $item = $(this);
+    navListItems.click(function (e) {
+        e.preventDefault();
+        var $target = $($(this).attr('href')),
+                $item = $(this);
 
-    if (!$item.hasClass('disabled')) {
-        navListItems.removeClass('btn-primary').addClass('btn-default');
-        $item.addClass('btn-primary');
-        allWells.hide();
-        $target.show();
-        $target.find('input:eq(0)').focus();
-    }
-});
-
-allNextBtn.click(function(){
-    var curStep = $(this).closest(".setup-content"),
-        curStepBtn = curStep.attr("id"),
-        nextStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().next().children("a"),
-        curInputs = curStep.find("input[type='text'],input[type='url']"),
-        isValid = true;
-
-    $(".form-group").removeClass("has-error");
-    for(var i=0; i<curInputs.length; i++){
-        if (!curInputs[i].validity.valid){
-            isValid = false;
-            $(curInputs[i]).closest(".form-group").addClass("has-error");
+        if (!$item.hasClass('disabled')) {
+            navListItems.removeClass('btn-primary').addClass('btn-default');
+            $item.addClass('btn-primary');
+            allWells.hide();
+            $target.show();
+            $target.find('input:eq(0)').focus();
         }
-    }
+    });
 
-    if (isValid)
-        nextStepWizard.removeAttr('disabled').trigger('click');
-});
+    allNextBtn.click(function(){
+        var curStep = $(this).closest(".setup-content"),
+            curStepBtn = curStep.attr("id"),
+            nextStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().next().children("a"),
+            curInputs = curStep.find("input[type='text'],input[type='url']"),
+            isValid = true;
 
-$('div.setup-panel div a.btn-primary').trigger('click');
-});
+        $(".form-group").removeClass("has-error");
+        for(var i=0; i<curInputs.length; i++){
+            if (!curInputs[i].validity.valid){
+                isValid = false;
+                $(curInputs[i]).closest(".form-group").addClass("has-error");
+            }
+        }
 
-
-
-
-
+        if (isValid) {
+            nextStepWizard.removeAttr('disabled').trigger('click');
+        }
+    });
 
     var x = document.getElementById("demo");
 
@@ -399,7 +392,6 @@ $('div.setup-panel div a.btn-primary').trigger('click');
         x.innerHTML = "Latitude: " + position.coords.latitude +
             "<br>Longitude: " + position.coords.longitude;
     }
-
 
     $('#country').change(function() {
         console.log('asa');
@@ -445,5 +437,16 @@ $('div.setup-panel div a.btn-primary').trigger('click');
             });
         }
     });
+    
+)};
+
 </script>
+
+
+
+
+
+
+
+
 @endpush
