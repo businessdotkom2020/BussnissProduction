a304288626d6f6244377eaa091919a072a85f273
businessdotkom2020 <businessdotkom2020@gmail.com>
2021-02-16 15:00:34 +0200

    My message

--- resources/views/Suppliers/edit.blade.php
+++ resources/views/Suppliers/edit.blade.php
@@ -269,47 +269,12 @@ $menu = false ;
 
 
                         <div class="ito-h col-xs-12">
-                            <h4>@lang('general.localization_info')</h4>
-                            </h4>
-                            <div class="form-group col-md-4 col-xs-12">
-                                <select style="width:100%" required id="country" name="country_id" class="form-control">
-
-                                    @foreach (\App\Models\Country::get() as $country)
-                                    <option {{ $supplier->country_id == $country->id ? 'selected' : ''}}
-                                        value="{{$country->id}}">{{$country->name}}</option>
-                                    @endforeach
-
-                                </select>
-                            </div>
-                            <div class="form-group col-md-4 col-xs-12">
 
 
-                                <select style="width:100%" required name="state_id" id="state" class="form-control">
-                                    <option selected disabled>@lang('general.state')</option>
-                                    @foreach (\App\Models\State::get() as $state)
-                                    <option {{ $supplier->state_id == $state->id ? 'selected' : ''}}
-                                        value="{{$state->id}}">{{$state->name}}</option>
-                                    @endforeach
-
-                                </select>
-                            </div>
-                            <div class="form-group col-md-4 col-xs-12">
-
-
-                                <select style="width:100%" required name="city_id" id="city" class="form-control">
-                                    <option selected disabled>@lang('general.city')</option>
-
-
-                                    @foreach (\App\Models\City::get() as $city)
-                                    <option {{ $supplier->city_id == $city->id ? 'selected' : ''}}
-                                        value="{{$city->id}}">{{$city->name}}</option>
-                                    @endforeach
-                                </select>
-                            </div>
                             <div class="form-group col-md-12 col-xs-12">
                                 <h4>@lang('general.street_num')</h4>
 
-                                <input required type="text" name="street_nom" placeholder="@lang('general.street_num')"
+                                <input required type="text" name="name" placeholder="@lang('general.name')"
                                     class="form-control">
                             </div>
                             <div class="form-group col-md-6 col-xs-12">
