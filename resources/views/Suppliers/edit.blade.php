19743c0efe51df3f1f96983d70a63c3e99993353
businessdotkom2020 <businessdotkom2020@gmail.com>
2021-02-20 14:40:22 +0200

    My message

--- resources/views/Suppliers/edit.blade.php
+++ resources/views/Suppliers/edit.blade.php
@@ -195,19 +195,19 @@
                     @foreach ($supplier->branchs as $branch)
 
                     <div class="ito-h col-xs-12 ubranch">
-                        <div class="form-group col-md-6 col-xs-12">
+                        <div class="form-group col-md-3 col-xs-12">
                             <h4>@lang('general.name')</h4>
                             <h5>@lang('general.name')</h5>
                         </div>
-                        <div class="form-group col-md-6 col-xs-12">
+                        <div class="form-group col-md-3 col-xs-12">
                             <h4>@lang('general.email')</h4>
                             <h5>@lang('general.email')</h5>
                         </div>
-                        <div class="form-group col-md-6 col-xs-12">
+                        <div class="form-group col-md-3 col-xs-12">
                             <h4>@lang('general.mobile')</h4>
                             <h5>@lang('general.mobile')</h5>
                         </div>
-                        <div class="form-group col-md-6 col-xs-12">
+                        <div class="form-group col-md-9 col-xs-12">
                             <iframe width="300" height="170" frameborder="0" scrolling="no" marginheight="0"
                                 marginwidth="0"
                                 src="https://maps.google.com/maps?q={{$branch->lat}},{{$branch->lang}}&hl=en&z=14&amp;output=embed">
