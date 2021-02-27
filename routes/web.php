9d006e7cfacf6f1c1e34d2571a2513915e053159
businessdotkom2020 <businessdotkom2020@gmail.com>
2021-02-27 17:11:32 +0200

    My message

--- routes/web.php
+++ routes/web.php
@@ -214,4 +214,4 @@ Route::get('/profile/update', 'web\SuppliersController@edit');
 Route::post('/update/supplier/profile', 'web\SuppliersController@update')->name('update_supplier');
 Route::post('/update/password', 'web\SuppliersController@update_password')->name('update_password');
 Route::get('/add/branch', 'web\BranchController@create_branch')->name('create_branch');
-Route::post('/add/branch', 'web\BranchController@add_branch')->name('store_branch');
+Route::post('/add/branch', 'web\BranchController@store')->name('store_branch');
