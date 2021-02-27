9ca844275dbf7b521ab2953b6107bba2c5f594be
businessdotkom2020 <businessdotkom2020@gmail.com>
2021-02-27 13:53:02 +0200

    My message

---
+++ resources/views/Suppliers/branches/create.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.main')
+@php $menu = false ;
+@endphp
+@push('styles')
+<link rel="stylesheet" href="{{ asset('/web/css/custom-save-supplier.css')}}">
+<link rel="stylesheet" href="{{ asset('/web/css/custom-register-supplier.css')}}">
+
+@endpush
+@section('content')
+
+<main class="main-content col-xs-12">
+    <div class="supp-prof-about col-xs-12">
+        <div class="container">
+
+
+        </div>
+    </div>
+</main>
+
+@endsection
+
+@push('scripts')
+
+@endpush
