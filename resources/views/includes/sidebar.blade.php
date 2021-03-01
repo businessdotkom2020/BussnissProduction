9b41e64fcfe5d487c52ca9e9b11006120bcc715b
businessdotkom2020 <businessdotkom2020@gmail.com>
2021-03-02 01:43:02 +0200

    My message

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -173,7 +173,7 @@
 
                             <li class="">
                                 <a href="{{url('/all-categories')}}">
-                                    <img src="{{url('/storage/allcat.png')}}" alt="">
+                                    <img src="{{url('/storage/categories/allcat.png')}}" alt="">
                                     {{__('general.others')}}
                                 </a>
 
