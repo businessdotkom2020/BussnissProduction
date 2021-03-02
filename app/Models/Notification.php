91504bf23fba8a38a53712331535e4e257f0c9ea
businessdotkom2020 <businessdotkom2020@gmail.com>
2021-03-02 15:12:12 +0200

    My message

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -7,23 +7,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class Notification extends Model
 {
-    
-    
-     /**
-         * Get the user that owns the phone.
+
+    public function getCreatedAtAttribute($date)
+    {
+        return  $date ? \Carbon\Carbon::parse($date)->format('H:i Y/m/d') : '19:17 2021/01/25';
+    }
+
+
+    public function getUpdatedAtAttribute($date)
+    {
+        return  $date ? \Carbon\Carbon::parse($date)->format('H:i Y/m/d') : '19:17 2021/01/25';
+    }
+
+    /**
+     * Get the user that owns the phone.
      */
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 
-     /**
-         * Get the user that owns the phone.
+    /**
+     * Get the user that owns the phone.
      */
     public function supplier()
     {
-        return $this->belongsTo(User::class,'supplier_id');
+        return $this->belongsTo(User::class, 'supplier_id');
     }
-    
-
 }
