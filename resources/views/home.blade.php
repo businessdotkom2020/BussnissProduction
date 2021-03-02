1364a429cdcb0c403dd0654286129fdeda51177d
businessdotkom2020 <businessdotkom2020@gmail.com>
2021-03-02 11:11:53 +0200

    My message

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -328,7 +328,7 @@ $menu = true ;
                             </a>
                             @if ($product->sale_price)
                             <span
-                                class="product-discount-label">{{number_format($product->price - ($product->price * ( $product->sale_price / 100) ) ,0)  .'%' }}</span>
+                                class="product-discount-label">{{number_format((($this->product/$this->product) * 100) ,0)  .'%'}}</span>
                             @endif
 
                             <ul class="product-links">
