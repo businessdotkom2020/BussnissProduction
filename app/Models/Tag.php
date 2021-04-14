fb35e20a12f3626867ea81244b87f35c56649cf8
businessdotkom2020 <businessdotkom2020@gmail.com>
2021-04-14 11:14:44 +0200

    fix

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -15,10 +15,7 @@ class Tag extends Model
 
 
 
-    public function getCreatedAtAttribute($date)
-    {
-        return  $date ? \Carbon\Carbon::parse($date)->format('H:i Y/m/d') : '19:17 2021/01/25';
-    }
+  
 
 
     public function products()
