553e6081ee02e09e509ddba2035ca68ac63da0bf
businessdotkom2020 <businessdotkom2020@gmail.com>
2021-04-15 10:23:04 +0200

    fix

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -686,7 +686,7 @@ function addPrice() {
                  <br>
                  <div id="prices">
 
-                     @foreach($product->prices as $price)
+                     @forelse($product->prices as $price)
                      <div class="form-group col-md-6 col-xs-12">
                          <h4>Price</h4>
                          <input value="{{$price->price}}" name="prices[{{$loop->index}}][price]" type="text" class="form-control">
@@ -699,7 +699,27 @@ function addPrice() {
                          <h4>Quantity To</h4>
                          <input value="{{$price->quantity_to}}" name="prices[{{$loop->index}}][quantity_to]" type="text" class="form-control">
                      </div>
-                     @endforeach
+                     @empty
+                     <div class="form-group col-md-5 col-xs-12">
+                         <h4>@lang('general.price')</h4>
+                         <input name="prices[0][price]" type="text" class="form-control">
+                     </div>
+                     <div class="form-group col-md-3 col-xs-12">
+                         <h4>@lang('general.quantity_from')</h4>
+                         <input name="prices[0][quantity_from]" type="number" class="form-control">
+                     </div>
+
+                     <div class="form-group col-md-3 col-xs-12">
+                         <h4>@lang('general.quantity_to')</h4>
+                         <input name="prices[0][quantity_to]" type="number" class="form-control">
+                     </div>
+                     <div class="form-group col-md-1 col-xs-12">
+
+                     </div>
+                     @error('product_price_list')
+                     <div class="alert" style="color:#a94442">{{ $message }}</div>
+                     @enderror
+                     @forelse
 
                      <!-- <div class="form-group col-md-5 col-xs-12">
                          <h4>@lang('general.price')</h4>
