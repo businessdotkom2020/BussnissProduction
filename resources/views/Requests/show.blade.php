7e5befaba3026a99a6a5dc97c44f63d0feb84510
businessdotkom2020 <businessdotkom2020@gmail.com>
2021-04-20 10:09:57 +0200

    fix

--- resources/views/Requests/show.blade.php
+++ resources/views/Requests/show.blade.php
@@ -8,15 +8,15 @@ $menu = false ;
 <div class="overlay-s"></div>
 <div class="jumpo col-xs-12" style="background-image: url({{ asset('/web/images/cover.jpg')}})">
     <div class="container">
-        <h3>request page name</h3>
+        <h3>{{$req->category->getTranslatedAttribute('name',\App::getLocale())}}</h3>
         <ul>
             <li>
                 <a href="{{url('/')}}">@lang('general.home')</a>
             </li>
             <li>
-                <a href="#">requests</a>
+                <a href="{{url('/requests')}}">@lang('general.requests')</a>
             </li>
-            <li>request name</li>
+            <li>{{$req->getTranslatedAttribute('name',\App::getLocale())}}</li>
         </ul>
     </div>
 </div>
@@ -31,8 +31,7 @@ $menu = false ;
                 <div class="block col-xs-12  col-md-8">
                     <div class="inner col-xs-12">
                         <div class="i-top col-xs-12">
-                            <span
-                                class="type-badge">{{$req->category->getTranslatedAttribute('name',\App::getLocale())}}</span>
+                            <span class="type-badge">{{$req->category->getTranslatedAttribute('name',\App::getLocale())}}</span>
                             <div class="i-slider owl-carousel owl-theme">
 
                                 @foreach(json_decode($req->images) as $img)
@@ -52,7 +51,7 @@ $menu = false ;
                             </div>
                             <div class="author">
                                 <img src="{{url('storage/'.$req->user->avatar)}}" alt="">
-                                <span>اسم التاجر يكتب هنا</span>
+                                <span>{{ $req->user->name}}</span>
                             </div>
 
                             <div class="cardo">
@@ -77,15 +76,11 @@ $menu = false ;
                                         </p>
                                     </div>
                                 </div>
-                                <a class="btn" href="{{url('supplier/'.$req->user_id)}}"
-                                    target="_blank">@lang('general.contact_supplier')</a>
+                                <a class="btn" href="{{url('supplier/'.$req->user_id)}}" target="_blank">@lang('general.contact_supplier')</a>
 
                             </div>
 
 
-                            <div class="time">
-                                <span>{{$req->created_at ? $req->created_at : null}}</span>
-                            </div>
                         </div>
                         <div class="i-footer col-xs-12">
                             <div class="title">
@@ -93,47 +88,33 @@ $menu = false ;
                             </div>
                             <div class="extra">
                                 <div class="prod-extra">
-                                    <a href="javascript:void(0)" id="fav-{{$req->id}}" title="add to favourite"
-                                        data-placement="top"
-                                        class="fav-{{$req->id}} {{$req->isFavorited() ? 'fav-active' : null  }}"
-                                        onclick="favtoggle({{$req->id }},{{Auth::user() ? Auth::user()->id : null}})">
+                                    <a href="javascript:void(0)" id="fav-{{$req->id}}" title="add to favourite" data-placement="top" class="fav-{{$req->id}} {{$req->isFavorited() ? 'fav-active' : null  }}" onclick="favtoggle({{$req->id }},{{Auth::user() ? Auth::user()->id : null}})">
                                         <i class="fa fa-heart"></i>
                                     </a>
-                                    <a href="javascript:void(0)" class="op-social" data-tool="tooltip" title="share"
-                                        data-placement="top">
+                                    <a href="javascript:void(0)" class="op-social" data-tool="tooltip" title="share" data-placement="top">
                                         <i class="fa fa-share-alt"></i>
                                     </a>
                                 </div>
                                 <div class="social shares">
-                                    <a href="#"
-                                        onclick="window.open('https://www.facebook.com/sharer/sharer.php?u='+encodeURIComponent(location.href),'facebook-share-dialog','width=626,height=436');return false;"
-                                        class="fb">
+                                    <a href="#" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u='+encodeURIComponent(location.href),'facebook-share-dialog','width=626,height=436');return false;" class="fb">
                                         <i class="fa fa-facebook"></i>
                                         share with facebook
                                     </a>
-                                    <a href="#"
-                                        onclick="window.open('https://twitter.com/intent/tweet?u='+encodeURIComponent(location.href),'facebook-share-dialog','width=626,height=436');return false;"
-                                        class="tw">
+                                    <a href="#" onclick="window.open('https://twitter.com/intent/tweet?u='+encodeURIComponent(location.href),'facebook-share-dialog','width=626,height=436');return false;" class="tw">
                                         <i class="fa fa-twitter"></i>
                                         share with twitter
                                     </a>
-                                    <a href="#"
-                                        onclick="window.open('https://www.linkedin.com/shareArticle?u='+encodeURIComponent(location.href),'facebook-share-dialog','width=626,height=436');return false;"
-                                        class="linkedin">
+                                    <a href="#" onclick="window.open('https://www.linkedin.com/shareArticle?u='+encodeURIComponent(location.href),'facebook-share-dialog','width=626,height=436');return false;" class="linkedin">
                                         <i class="fa fa-linkedin"></i>
                                         share with linkedin
                                     </a>
                                 </div>
-                                <a class="btn" href="{{url('supplier/'.$req->user_id)}}"
-                                    target="_blank">@lang('general.contact_supplier')</a>
+                                <!-- <a class="btn" href="{{url('supplier/'.$req->user_id)}}" target="_blank">@lang('general.contact_supplier')</a> -->
 
-<<<<<<< HEAD
-=======
 
                                 <div class="time">
                                     <span>{{$req->created_at ? $req->created_at : null}}</span>
                                 </div>
->>>>>>> 724b40bcb919e73e7638439759a9b73421283f15
                             </div>
                         </div>
                     </div>
@@ -142,8 +123,7 @@ $menu = false ;
 
 
                     <div class="req-form col-md-12 col-xs-12">
-                        <form action="{{route('add_product_review',['type' => 'request','id' => $req->id])}}"
-                            method="POST" class="rating-form">
+                        <form action="{{route('add_product_review',['type' => 'request','id' => $req->id])}}" method="POST">
                             @csrf
 
                             <div class="form-group">
@@ -151,29 +131,24 @@ $menu = false ;
                                 <div class="rate-stars" style="text-align: right;">
                                     <div class="stars">
 
-                                        <input value="1" class="star star-5" id="star-5" type="radio" required
-                                            name="rating" />
+                                        <input value="1" class="star star-5" id="star-5" type="radio" required name="stars" />
                                         <label class="star star-5" for="star-5"></label>
-                                        <input value="2" class="star star-4" id="star-4" type="radio" name="rating" />
+                                        <input value="2" class="star star-4" id="star-4" type="radio" name="stars" />
                                         <label class="star star-4" for="star-4"></label>
-                                        <input value="3" class="star star-3" id="star-3" type="radio" name="rating" />
+                                        <input value="3" class="star star-3" id="star-3" type="radio" name="stars" />
                                         <label class="star star-3" for="star-3"></label>
-                                        <input value="4" class="star star-2" id="star-2" type="radio" name="rating" />
+                                        <input value="4" class="star star-2" id="star-2" type="radio" name="stars" />
                                         <label class="star star-2" for="star-2"></label>
-                                        <input value="5" class="star star-1" id="star-1" type="radio" name="rating" />
+                                        <input value="5" class="star star-1" id="star-1" type="radio" name="stars" />
                                         <label class="star star-1" for="star-1"></label>
 
                                     </div>
-                                    <!-- <p>
+                                    <p>
                                         برجاء اضافه تقييم قبل التعليق
-                                    </p> -->
+                                    </p>
                                 </div>
 
-                                <textarea class="form-control" name="comment" placeholder="@lang('general.add_review')"
-                                    id='review'></textarea>
-
-
-                                <span class='error'>error Msg</span>
+                                <textarea class="form-control" name="comment" placeholder="@lang('general.add_review')"></textarea>
                                 <button type="submit" class="btn" id="req-f-btn">@lang('general.add_review')</button>
                             </div>
                         </form>
@@ -200,8 +175,7 @@ $menu = false ;
                             </div>
                             @auth
                             @if(\Auth::user()->id == $review->user_id)
-                            <a href="{{url('review/'.$review->id.'/delete')}}" style="    float: inline-end;"><i
-                                    class="fa fa-trash" aria-hidden="true"></i></a>
+                            <a href="{{url('review/'.$review->id.'/delete')}}" style="    float: inline-end;"><i class="fa fa-trash" aria-hidden="true"></i></a>
                             @endif
                             @endauth
                             <div class="r-data">
@@ -254,44 +228,15 @@ $menu = false ;
 
 @push('scripts')
 <script>
-    // $("#req-f-btn").click(function (e) {
-    //     if ($("[type=radio]").not(':checked')[0]) {
-    //         e.preventDefault();
-    //         //e.stopPropagation();
-    //         alert(' برجاء اضافه تقييم قبل التعليق');
+    $("#req-f-btn").click(function(e) {
+        if ($("[type=radio]").not(':checked')[0]) {
+            e.preventDefault();
+            e.stopPropagation();
+            alert(' برجاء اضافه تقييم قبل التعليق');
 
-    //     } //end if
-    // });
-
-
-
-
-
-
-
-
-    $('#req-f-btn').on('click', function () {
-        var rating = $("input[name=rating]:checked").attr('value');
-        var review = $('#review').val();
-        if (rating == '0') {
-            $('.error').html('Please select rating');
-
-        } else if (name == '') {
-            $('.error').html('Please enter name');
-        } else if (name.length <= 2 || name.legth >= 50) {
-            $('.error').html('Please enter name in less than 50 Characters');
-        } else if (review == '') {
-            $('.error').html('Please enter review');
-        } else if (review.length <= 2 || review.legth >= 250) {
-            $('.error').html('Please enter review in less than 250 Characters');
-        } else {
-            $('.error').html('');
-            alert(rating + '|' + name + '|' + review);
-            $('.rating-form').hide();
-            $('.rating-success').addClass('active');
-        }
-    })
-
+        } //end if
+    });
 </script>
+
 
 @endpush
