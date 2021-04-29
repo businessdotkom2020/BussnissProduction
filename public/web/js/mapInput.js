9414131f2807f68f03fd8a66eb982bfbb7b87981
businessdotkom2020 <businessdotkom2020@gmail.com>
2021-04-29 16:49:58 +0200

    fix

--- public/web/js/mapInput.js
+++ public/web/js/mapInput.js
@@ -10,7 +10,39 @@
           zoom: 13
         });
 
+        infoWindow = new google.maps.InfoWindow();
 
+        const locationButton = document.createElement("button");
+        locationButton.textContent = "Pan to Current Location";
+        locationButton.classList.add("custom-map-control-button");
+      
+        map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+      
+        locationButton.addEventListener("click", () => {
+          // Try HTML5 geolocation.
+          if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+              (position: Position) => {
+                const pos = {
+                  lat: position.coords.latitude,
+                  lng: position.coords.longitude,
+                };
+      
+                infoWindow.setPosition(pos);
+                infoWindow.setContent("Location found.");
+                infoWindow.open(map);
+                map.setCenter(pos);
+              },
+              () => {
+                handleLocationError(true, infoWindow, map.getCenter()!);
+              }
+            );
+          } else {
+            // Browser doesn't support Geolocation
+            handleLocationError(false, infoWindow, map.getCenter()!);
+          }
+        });
+      }
         
   var clickmarker = new google.maps.Marker({
      draggable: true
