20c3045608d3bb28005146361d8ee2c84ca59d13
businessdotkom2020 <businessdotkom2020@gmail.com>
2021-04-29 14:57:40 +0200

    fix

--- resources/views/auth/supplier_register.blade.php
+++ resources/views/auth/supplier_register.blade.php
@@ -327,14 +327,14 @@ $menu = false ;
 @push('scripts')
 @parent
 
-<!-- <script src="{{ asset('/web/js/select2.full.min.js')}}"></script>
+<script src="{{ asset('/web/js/select2.full.min.js')}}"></script>
 <script src="{{ asset('/web/js/mapInput.js')}}"></script>
 
 
 
 <script
     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBcP3JChqWSCHtsjW4e_Ug8h0htjiIhcHw&libraries=places&callback=initMap"
-    async defer></script> -->
+    async defer></script>
 <!--<script src="https://maps.googleapis.com/maps/api/js?key={{ env('GOOGLE_MAPS_API_KEY') }}&libraries=places&callback=initialize" async defer></script>-->
 <!--<script src="https://maps.googleapis.com/maps/api/js?key=&callback=initMap" async defer></script>-->
 
