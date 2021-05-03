0daa9dd80cabaeb5e66259d442a3718d33f832c3
businessdotkom2020 <businessdotkom2020@gmail.com>
2021-05-03 16:13:15 +0200

    ff

--- resources/views/auth/supplier_register.blade.php
+++ resources/views/auth/supplier_register.blade.php
@@ -320,7 +320,7 @@ $menu = false ;
 
 @push('scripts')
 <script>
-    var current_location = {{ trans('general.current_location') }};
+    var current_location = @json( __('general.current_location') );
 </script>
 
 <script src="{{ asset('/web/js/select2.full.min.js')}}"></script>
