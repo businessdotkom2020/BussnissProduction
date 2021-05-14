2f459aa590e646c3e7d2ff0b170afb0f686a4c05
businessdotkom2020 <businessdotkom2020@gmail.com>
2021-05-14 19:58:42 +0200

    f

--- resources/views/auth/supplier_steps/information.blade.php
+++ resources/views/auth/supplier_steps/information.blade.php
@@ -7,7 +7,7 @@
             @error('supplier_name')
             <div class="alert" style="color:#a94442">{{ $message }}</div>
             @enderror
-            <span class="error-msg supplier_name_err">هذا الحقل مطلوب.</span>
+            <span class="error-msg supplier_name_err"></span>
         </div>
 
 
@@ -18,21 +18,21 @@
             @error('email')
             <div class="alert" style="color:#a94442">{{ $message }}</div>
             @enderror
-            <span class="error-msg email_err">هذا الحقل مطلوب.</span>
+            <span class="error-msg email_err"></span>
         </div>
         <div class="form-group col-md-6 col-xs-12">
             <input type="text" required name="mobile" placeholder="@lang('general.mobile')" class="form-control">
             @error('mobile')
             <div class="alert" style="color:#a94442">{{ $message }}</div>
             @enderror
-            <span class="error-msg">هذا الحقل مطلوب.</span>
+            <span class="error-msg"></span>
         </div>
         <div class="form-group col-md-6 col-xs-12">
             <input type="text" required name="hot_number" placeholder="@lang('general.hot_line')" class="form-control">
             @error('hot_line')
             <div class="alert" style="color:#a94442">{{ $message }}</div>
             @enderror
-            <span class="error-msg hot_number_err">هذا الحقل مطلوب.</span>
+            <span class="error-msg hot_number_err"></span>
         </div>
 
         <div class="form-group col-md-12 col-xs-12">
@@ -46,7 +46,7 @@
                 </option>
                 @endforeach
             </select>
-            <span class="error-msg ml450 category_ids_err">هذا الحقل مطلوب.</span>
+            <span class="error-msg ml450 category_ids_err"></span>
             @error('category_ids')
             <div class="alert category_ids_err" style="color:#a94442">{{ $message }}</div>
             @enderror
@@ -57,7 +57,7 @@
             @error('password')
             <div class="alert" style="color:#a94442">{{ $message }}</div>
             @enderror
-            <span class="error-msg">هذا الحقل مطلوب.</span>
+            <span class="error-msg"></span>
         </div>
         <div class="form-group col-md-6 col-xs-12">
             <input type="password" required name="password_confirmation"
@@ -65,7 +65,7 @@
             @error('password_confirmation')
             <div class="alert" style="color:#a94442">{{ $message }}</div>
             @enderror
-            <span class="error-msg">هذا الحقل مطلوب.</span>
+            <span class="error-msg"></span>
         </div>
     </div>
     <div class="clearfix"></div>
