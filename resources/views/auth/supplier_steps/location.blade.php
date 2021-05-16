4a40aa464b1d90f7ac9b93ff0a81ca8510b4af4f
businessdotkom2020 <businessdotkom2020@gmail.com>
2021-05-16 22:11:54 +0200

    fix

--- resources/views/auth/supplier_steps/location.blade.php
+++ resources/views/auth/supplier_steps/location.blade.php
@@ -1,44 +1,139 @@
 <fieldset>
-    <h3>@lang('general.background_image')</h3>
+    <h3>@lang('general.localization_info')</h3>
     <div class="form-row">
-        <div class="form-group col-xs-12">
+        <div class="form-group col-md-4 col-xs-12">
+            <select required id="country" name="country_id" class="form-control error-va">
+                <option selected disabled>@lang('general.country')</option>
 
-            <div class="prof-img">
-                <div class="prof-change-btn">
-                    <label>
-                        <span> @lang('general.cheange')</span>
-                    </label>
-                </div>
-                <label>
-                    <i class="fa fa-camera"></i>
-                    <input name="store_background" type="file"
-                        onchange="document.getElementById('blah').src = window.URL.createObjectURL(this.files[0])">
+                @foreach (\App\Models\Country::get() as $country)
+                <option value="{{$country->id}}">{{$country->name}}</option>
+                @endforeach
 
-                    <img src="{{ asset('/web/images/cover.jpg')}}" id="blah" alt="your image">
-                </label>
-                @error('store_background')
-                <div class="alert" style="color:#a94442">{{ $message }}</div>
-                @enderror
+            </select>
+
+            @error('country_id')
+            <div class="alert" style="color:#a94442">{{ $message }}</div>
+            @enderror
+            <span class="error-msg">هذا الحقل مطلوب.</span>
+        </div>
+        <div class="form-group col-md-4 col-xs-12">
+            <select required name="state_id" id="state" class="form-control error-va">
+                <option selected disabled>@lang('general.state')</option>
+            </select>
+
+            @error('state_id')
+            <div class="alert" style="color:#a94442">{{ $message }}</div>
+            @enderror
+            <span class="error-msg">هذا الحقل مطلوب.</span>
+        </div>
+        <div class="form-group col-md-4 col-xs-12">
+            <select required name="city_id" id="city" class="form-control error-va">
+                <option selected disabled>@lang('general.city')</option>
+            </select>
+
+            @error('city_id')
+            <div class="alert" style="color:#a94442">{{ $message }}</div>
+            @enderror
+            <span class="error-msg">هذا الحقل مطلوب.</span>
+        </div>
+        <div class="form-group col-md-12 col-xs-12">
+            <input required type="text" name="street_nom" placeholder="@lang('general.street_num')"
+                class="form-control error-va">
+            @error('street_nom')
+            <div class="alert" style="color:#a94442">{{ $message }}</div>
+            @enderror
+            <span class="error-msg">هذا الحقل مطلوب.</span>
+        </div>
+
+        <div class="form-group col-md-12 col-xs-12">
+            <input required type="text" name="zip_code" placeholder="@lang('general.zip_code')"
+                class="form-control error-va">
+            @error('zip_code')
+            <div class="alert" style="color:#a94442">{{ $message }}</div>
+            @enderror
+            <span class="error-msg">هذا الحقل مطلوب.</span>
+        </div>
+        <div class="form-group col-md-12 col-xs-12">
+            <input required type="text" name="address" placeholder="@lang('general.address_spec')"
+                class="form-control error-va">
+            @error('address_spec')
+            <div class="alert" style="color:#a94442">{{ $message }}</div>
+            @enderror
+            <span class="error-msg">هذا الحقل مطلوب.</span>
+        </div>
+        <div class="form-group col-md-12 col-xs-12">
+            <!--<input type="text" placeholder="@lang('general.map_location')" class="form-control">-->
+            <!--<a href="#" class="btn btn-review" data-toggle="modal" onclick="initMap()"
+            data-target="#review-pop">@lang('general.map_location')</a>-->
+
+        </div>
+        <div class="form-group col-md-12 col-xs-12 map-location-block">
+            <div class="rate-title">
+                <h3>@lang('general.map_location')</h3>
             </div>
-            <div class="prof-img pic">
-                <div class="prof-change-btn">
-                    <label>
-                        <span>@lang('general.cheange_profile_image')</span>
-                    </label>
-                </div>
-                <label>
-                    <i class="fa fa-camera"></i>
+            <div class="rate-stars">
 
-                    <input name="store_image" type="file"
-                        onchange="document.getElementById('blah1').src = window.URL.createObjectURL(this.files[0])">
-                    <img src="{{ url('storage/users/default_company.png')}}" id="blah1" alt="your image">
-                </label>
-                @error('store_image')
-                <div class="alert" style="color:#a94442">{{ $message }}</div>
-                @enderror
+                <div class="col-md-12 col-xs-12 m10_b">
+
+                    <div id="pac-container">
+                        <input id="pac-input" type="text" placeholder="Enter a location">
+                    </div>
+
+                </div>
+
+                <div class="col-md-6 col-xs-12 ">
+
+                    <div class="form-group">
+                        <input type="hidden" oninput="initMap()" value="31.3540494" class="form-control input-sm"
+                            name="lat" id="lat">
+                    </div>
+                </div>
+                <div class="col-md-6 col-xs-12 ">
+
+                    <div class="form-group">
+                        <input type="hidden" oninput="initMap()" value="31.6841419" class="form-control input-sm"
+                            name="lng" id="lng">
+                    </div>
+                </div>
+
+
+            </div>
+            <div class="rate-form">
+
+
+                <div class="pac-card" id="pac-card">
+                    <div>
+                        <div id="title">
+                            Autocomplete search
+                        </div>
+                        <div id="type-selector" class="pac-controls">
+                            <input type="radio" name="type" id="changetype-all" checked="checked">
+                            <label for="changetype-all">All</label>
+
+                            <input type="radio" name="type" id="changetype-establishment">
+                            <label for="changetype-establishment">Establishments</label>
+
+                            <input type="radio" name="type" id="changetype-address">
+                            <label for="changetype-address">Addresses</label>
+
+                            <input type="radio" name="type" id="changetype-geocode">
+                            <label for="changetype-geocode">Geocodes</label>
+                        </div>
+                        <div id="strict-bounds-selector" class="pac-controls">
+                            <input type="checkbox" id="use-strict-bounds" value="">
+                            <label for="use-strict-bounds">Strict Bounds</label>
+                        </div>
+                    </div>
+
+                </div>
+                <div id="map" style="width:100%;height:330px; "></div>
+
+
             </div>
         </div>
+
     </div>
     <div class="clearfix"></div>
-    <button type="button" class="next action-button next11">التالي</button>
+    <button type="button" class="action-button previous previous_button">رجوع</button>
+    <button type="submit" class="action-button">@lang('general.register')</button>
 </fieldset>
