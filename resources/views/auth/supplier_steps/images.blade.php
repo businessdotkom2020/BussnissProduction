70cfb28ce49e1aa2a7444b51260abf62ef5e6843
storeseg <84464012+storeseg@users.noreply.github.com>
2021-05-20 07:16:42 +0200

    fff

--- resources/views/auth/supplier_steps/images.blade.php
+++ resources/views/auth/supplier_steps/images.blade.php
@@ -17,9 +17,9 @@
                     <img src="{{ asset('/web/images/cover.jpg')}}" id="store_background" alt="your image">
                 </label>
 
-                <span class="error-msg store_background_err"></span>
 
             </div>
+            <span class="error-msg store_background_err"></span>
 
             <div class="prof-img pic">
                 <div class="prof-change-btn">
@@ -34,9 +34,9 @@
                         onchange="document.getElementById('store_image').src = window.URL.createObjectURL(this.files[0])">
                     <img src="{{ url('storage/users/default_company.png')}}" id="store_image" alt="your image">
                 </label>
-                <span class="error-msg store_image_err"></span>
 
             </div>
+            <span class="error-msg store_image_err"></span>
         </div>
     </div>
     <div class="clearfix"></div>
