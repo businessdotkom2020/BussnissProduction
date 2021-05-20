240dbb64962134b2e96c739f47f1ec13d58c5226
storeseg <84464012+storeseg@users.noreply.github.com>
2021-05-20 05:01:31 +0200

    fff

--- resources/views/auth/supplier_steps/location.blade.php
+++ resources/views/auth/supplier_steps/location.blade.php
@@ -4,36 +4,36 @@
 
         {{--  Country Input  --}}
         <div class="form-group col-md-4 col-xs-12">
-            <select required id="country_id" class="form-control error-va">
+            <select required id="country_id" name="country_id" class="form-control error-va">
                 <option selected disabled>@lang('general.country')</option>
                 @foreach (\App\Models\Country::get() as $country)
                 <option value="{{$country->id}}">{{$country->name}}</option>
                 @endforeach
             </select>
-            <input type="hidden" name="country_id" id="country_id_value">
-            <span class="error-msg  country_id_err"></span>
+
+            <span class="error-msg  state_id_err"></span>
 
         </div>
 
         {{--  State Input  --}}
 
         <div class="form-group col-md-4 col-xs-12">
-            <select required " id=" state_id" class="form-control error-va">
+            <select required name="state_id" id="state_id" class="form-control error-va">
                 <option selected disabled>@lang('general.state')</option>
             </select>
-            <input type="hidden" name="state_id" id="state_id_value">
-            <span class="error-msg  state_id_err"></span>
+
+            <span class="error-msg  city_id_err"></span>
 
         </div>
 
         {{--  City Input  --}}
 
         <div class="form-group col-md-4 col-xs-12">
-            <select required id="city_id" class="form-control error-va">
+            <select required name="city_id" id="city_id" class="form-control error-va">
                 <option selected disabled>@lang('general.city')</option>
             </select>
-            <input type="hidden" name="city_id" id="city_id_value">
-            <span class="error-msg  city_id_err"></span>
+
+            <span class="error-msg  country_id_err"></span>
 
         </div>
 
