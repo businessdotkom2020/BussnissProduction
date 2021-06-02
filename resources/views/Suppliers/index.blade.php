50dd44b97be310eb71368df0eb572d2b514e4504
storeseg <84464012+storeseg@users.noreply.github.com>
2021-06-03 01:42:56 +0200

    fix

--- resources/views/Suppliers/index.blade.php
+++ resources/views/Suppliers/index.blade.php
@@ -30,6 +30,138 @@ $menu = false ;
             <div class="suppliers-archive col-xs-12">
                 <div class="container">
 
+                    <div class="row">
+                        <div id="primary" class="content-area col-md-12">
+                            <main id="main" class="site-main">
+                                <article id="post-475" class="post-475 page type-page status-publish hentry">
+                                    <div class="entry-content">
+                                        <div id="dokan-store-listing-filter-wrap">
+                                            <div class="left">
+                                                <p class="item store-count">
+                                                    Total store showing: {{count($suppliers)}}
+                                                </p>
+                                            </div>
+                                            <div class="right">
+                                                <div class="item">
+                                                    <div class="dokan-icons">
+                                                        <div class="dokan-icon-div"></div>
+                                                        <div class="dokan-icon-div"></div>
+                                                        <div class="dokan-icon-div"></div>
+                                                    </div>
+                                                    <button
+                                                        class="dokan-store-list-filter-button dokan-btn dokan-btn-theme">
+                                                        Filter </button>
+                                                </div>
+                                                <form name="stores_sorting" class="sort-by item" method="get">
+                                                    <label>Sort by:</label>
+                                                    <select name="stores_orderby" id="stores_orderby"
+                                                        aria-label="Sort by">
+                                                        <option value="most_recent">Most Recent</option>
+                                                        <option value="total_orders">Most Popular</option>
+                                                    </select>
+                                                </form>
+                                                <div class="toggle-view item">
+                                                    <span class="dashicons dashicons-screenoptions"
+                                                        data-view="grid-view"></span>
+                                                    <span class="dashicons dashicons-menu-alt"
+                                                        data-view="list-view"></span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <form role="store-list-filter" name="dokan_store_lists_filter_form"
+                                            id="dokan-store-listing-filter-form-wrap" style="display: none">
+                                            <div class="store-search grid-item">
+                                                <input type="search" class="store-search-input"
+                                                    name="dokan_seller_search" placeholder="Search Vendors">
+                                            </div>
+                                            <div class="apply-filter">
+                                                <button id="cancel-filter-btn"
+                                                    class="dokan-btn dokan-btn-theme">Cancel</button>
+                                                <button id="apply-filter-btn" class="dokan-btn dokan-btn-theme"
+                                                    type="submit">Apply</button>
+                                            </div>
+                                        </form>
+                                        <div id="dokan-seller-listing-wrap" class="grid-view">
+                                            <div class="seller-listing-content">
+                                                <ul class="dokan-seller-wrap">
+
+
+
+
+                                                    <li class="dokan-single-seller woocommerce coloum-3 ">
+                                                        <div class="store-wrapper">
+                                                            <div class="store-header">
+                                                                <div class="store-banner">
+                                                                    <a href="gopro.html">
+                                                                        <img src="images/martfury-store-list">
+                                                                    </a>
+                                                                </div>
+                                                            </div>
+                                                            <div class="store-content ">
+                                                                <div class="store-data-container">
+                                                                    <div class="featured-favourite">
+                                                                        <div class="featured-label">Featured</div>
+                                                                    </div>
+                                                                    <div class="store-data">
+                                                                        <h2><a href="gopro.html">Go Pro</a></h2>
+                                                                        <div class="dokan-seller-rating"
+                                                                            title="Rated 4.10 out of 5">
+                                                                            <i
+                                                                                class="dashicons dashicons-star-filled"></i><i
+                                                                                class="dashicons dashicons-star-filled"></i><i
+                                                                                class="dashicons dashicons-star-filled"></i><i
+                                                                                class="dashicons dashicons-star-filled"></i><i
+                                                                                class="dashicons dashicons-star-half"></i>
+                                                                            <p class="rating">
+                                                                                4.10 out of 5
+                                                                            </p>
+                                                                        </div>
+                                                                        <p class="store-address"><span class="street_1">
+                                                                                326 Orchard
+                                                                                Str,</span><br><span class="state">
+                                                                                Riau,</span><span class="country">
+                                                                                Indonesia </span>
+                                                                        </p>
+                                                                        <p class="store-phone">
+                                                                            <i class="fa fa-phone"
+                                                                                aria-hidden="true"></i> (+043)
+                                                                            88-637-3300
+                                                                        </p>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                            <div class="store-footer">
+                                                                <div class="seller-avatar">
+                                                                    <img alt=""
+                                                                        src="https://secure.gravatar.com/avatar/849845cf0dfe61def7c949a35a104ac8?s=150&amp;d=mm&amp;r=g"
+                                                                        class="avatar avatar-150 photo lazyloaded"
+                                                                        height="150" width="150"
+                                                                        srcset="https://secure.gravatar.com/avatar/849845cf0dfe61def7c949a35a104ac8?s=300&amp;d=mm&amp;r=g 2x"
+                                                                        data-ll-status="loaded">
+                                                                    <noscript><img alt=""
+                                                                            src="images/avatar-849845cf0dfe61def7c949a35a104ac8"
+                                                                            srcset="https://secure.gravatar.com/avatar/849845cf0dfe61def7c949a35a104ac8?s=300&amp;d=mm&amp;r=g 2x"
+                                                                            class="avatar avatar-150 photo" height="150"
+                                                                            width="150"></noscript>
+                                                                </div>
+                                                                <a href="gopro.html" title="Visit Store">
+                                                                    <span
+                                                                        class="dashicons dashicons-arrow-right-alt2 dokan-btn-theme dokan-btn-round"></span>
+                                                                </a>
+                                                            </div>
+                                                        </div>
+                                                    </li>
+
+
+                                                    <div class="dokan-clearfix"></div>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </article>
+                            </main>
+                        </div>
+                    </div>
 
                     <div class="g-body col-xs-12">
                         @foreach($suppliers as $supplier)
