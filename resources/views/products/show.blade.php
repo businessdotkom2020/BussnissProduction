bcf05d4fb56c6c063c737580ddfe3056107d15bb
Mayada95 <mayadabadran95@gmail.com>
2021-06-05 17:37:36 +0200

    fix add order

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -411,11 +411,14 @@ $menu = false ;
                                 <a href="#" data-toggle="tab" data-target="#t0">Description</a>
                             </li>
                             <li class="">
-                                <a href="#" data-toggle="tab" data-target="#t1">@lang('general.pricing_plans')</a>
+                                <a href="#" data-toggle="tab" data-target="#t1">Specification</a>
+                            </li>
+                            <li class="">
+                                <a href="#" data-toggle="tab" data-target="#t2">@lang('general.pricing_plans')</a>
                             </li>
 
                             <li>
-                                <a href="#" data-toggle="tab" data-target="#t2">@lang('general.reviews')</a>
+                                <a href="#" data-toggle="tab" data-target="#t3">@lang('general.reviews')</a>
                             </li>
 
                         </ul>
@@ -467,7 +470,90 @@ $menu = false ;
                                     </p>
                                 </div>
                             </div>
-                            <div class="tab-pane fade active in" id="t1">
+
+                            <div class="tab-pane fade" id="t1">
+
+                                <div class="product-specification table-responsive">
+                                    <h3>Technical Specifications</h3>
+                                    <table class="table">
+                                        <tbody>
+                                            <tr>
+                                                <td>Brand</td>
+                                                <td>Apple</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Item Height</td>
+                                                <td>18 Millimeters</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Item Width</td>
+                                                <td>31.4 Centimeters</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Screen Size</td>
+                                                <td>13 Inches</td>
+                                            </tr>
+                                            <tr class="size-weight">
+                                                <td>Item Weight</td>
+                                                <td>1.6 Kg</td>
+                                            </tr>
+                                            <tr class="size-weight">
+                                                <td>Product Dimensions</td>
+                                                <td>21.9 x 31.4 x 1.8 cm</td>
+                                            </tr>
+                                            <tr class="item-model-number">
+                                                <td>Item model number</td>
+                                                <td>MF841HN/A</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Processor Brand</td>
+                                                <td>Intel</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Processor Type</td>
+                                                <td>Core i5</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Processor Speed</td>
+                                                <td>2.9 GHz</td>
+                                            </tr>
+                                            <tr>
+                                                <td>RAM Size</td>
+                                                <td>8 GB</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Hard Drive Size</td>
+                                                <td>512 GB</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Hard Disk Technology</td>
+                                                <td>Solid State Drive</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Graphics Coprocessor</td>
+                                                <td>Intel Integrated Graphics</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Graphics Card Description</td>
+                                                <td>Integrated Graphics Card</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Hardware Platform</td>
+                                                <td>Mac</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Operating System</td>
+                                                <td>Mac OS</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Average Battery Life (in hours)</td>
+                                                <td>9</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                            <div class="tab-pane fade" id="t2">
 
                                 <div class="product-price-p table-responsive">
                                     <table class="table table-bordered">
@@ -495,7 +581,7 @@ $menu = false ;
                                     </table>
                                 </div>
                             </div>
-                            <div class="tab-pane fade" id="t2">
+                            <div class="tab-pane fade" id="t3">
 
                                 @foreach($product->reviews as $review)
 
