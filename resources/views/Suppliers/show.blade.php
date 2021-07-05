3eec998f4d18e5736deaa38a7384c646236d5bc6
storeseg <84464012+storeseg@users.noreply.github.com>
2021-07-05 19:04:19 +0200

    as

--- resources/views/Suppliers/show.blade.php
+++ resources/views/Suppliers/show.blade.php
@@ -78,10 +78,17 @@ $menu = false ;
                     <a href="blog.html" class="d-lg-show">Blog</a>
                     <a href="contact-us.html" class="d-lg-show">Contact Us</a>
                     <a href="my-account.html" class="d-lg-show">My Account</a>
+
                     <a href="assets/ajax/login.html" class="d-lg-show login sign-in"><i class="w-icon-account"></i>Sign
                         In</a>
                     <span class="delimiter d-lg-show">/</span>
                     <a href="assets/ajax/login.html" class="ml-0 d-lg-show login register">Register</a>
+                    <a href="assets/ajax/login.html" class="d-lg-show login sign-in"><i class="w-icon-store"></i>Sign
+                        In</a>
+                    <span class="delimiter d-lg-show">/</span>
+                    <a href="assets/ajax/login.html" class="ml-0 d-lg-show login register">Register</a>
+
+
                 </div>
             </div>
         </div>
