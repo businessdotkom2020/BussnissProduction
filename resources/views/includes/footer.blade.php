3e0142c91667317a50340f1e91a96027b00a06c6
storeseg <84464012+storeseg@users.noreply.github.com>
2021-07-05 17:56:27 +0200

    as

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,4 +1,4 @@
-<!-- Start of Footer -->
+{{-- <!-- Start of Footer -->
 <footer class="footer appear-animate" data-animation-options="{
     'name': 'fadeIn'
 }">
@@ -198,10 +198,10 @@
     <!-- End of .mobile-menu-close -->
 
 
-</div>
+</div> --}}
 
 
-{{-- <footer class="main-footer col-xs-12">
+<footer class="main-footer col-xs-12">
     <div class="ocean">
         <div class="wave"></div>
         <div class="wave"></div>
@@ -211,8 +211,8 @@
         <div class="container">
             <div class="f-item  f-logo-item col-md-3 col-sm-3 col-xs-12">
                 <img src="{{ asset('/web/images/splash_logo.png')}}" alt="">
-</div>
-<!-- <div class="f-item col-md-2 col-md-offset-1  col-sm-3 col-xs-12">
+            </div>
+            <!-- <div class="f-item col-md-2 col-md-offset-1  col-sm-3 col-xs-12">
                 <h4>{{__('general.my_profile')}}</h4>
                 <ul>
                     <li>
@@ -226,69 +226,69 @@
                     </li>
                 </ul>
             </div> -->
-<div class="f-item col-md-3 col-sm-3 col-xs-12">
-    <h4>@lang('general.fast_links')</h4>
-    <ul>
-        <li>
-            <a href="{{url('/')}}">{{__('general.home')}}</a>
-        </li>
-        <li>
-            <a href="{{url('/about')}}">{{__('general.about')}}</a>
-        </li>
-        <li>
-            <a href="{{url('/services')}}">{{__('general.services')}}</a>
-        </li>
+            <div class="f-item col-md-3 col-sm-3 col-xs-12">
+                <h4>@lang('general.fast_links')</h4>
+                <ul>
+                    <li>
+                        <a href="{{url('/')}}">{{__('general.home')}}</a>
+                    </li>
+                    <li>
+                        <a href="{{url('/about')}}">{{__('general.about')}}</a>
+                    </li>
+                    <li>
+                        <a href="{{url('/services')}}">{{__('general.services')}}</a>
+                    </li>
 
 
-        <li>
-            <a href="{{url('/privacy')}}">{{__('general.privacy')}}</a>
-        </li>
-        <li>
-            <a href="{{url('/terms')}}">{{__('general.terms')}}</a>
-        </li>
-    </ul>
-</div>
-
-<div class="f-item col-md-3 col-sm-3 col-xs-12">
-    <h4>social</h4>
-    <div class="social-f">
-        <a href="{{\App\Models\AppSetting::first()->facebook}}">
-            <i class="fa fa-facebook"></i>
-        </a>
-        <a href="{{\App\Models\AppSetting::first()->twitter}}">
-            <i class="fa fa-twitter"></i>
-        </a>
-        <a href="{{\App\Models\AppSetting::first()->linked_in}}">
-            <i class="fa fa-linkedin"></i>
-        </a>
-        <a href="{{\App\Models\AppSetting::first()->instagram}}">
-            <i class="fa fa-instagram"></i>
-        </a>
-        <a href="{{\App\Models\AppSetting::first()->whatsapp}}">
-            <i class="fa fa-whatsapp"></i>
-        </a>
-    </div>
-    <div class="f-form">
-        <h4>@lang('general.newsletter')</h4>
-        <p>@lang('general.newsletter_benefits')</p>
-        <form action="{{route('add_subscriber')}}" method="POST">
-            @csrf
-            <div class="form-group">
-                <input type="email" name="email" required class="form-control" placeholder="your email">
-                <button class="footer-btn" type="submit"><i class="fa fa-paper-plane-o"></i></button>
+                    <li>
+                        <a href="{{url('/privacy')}}">{{__('general.privacy')}}</a>
+                    </li>
+                    <li>
+                        <a href="{{url('/terms')}}">{{__('general.terms')}}</a>
+                    </li>
+                </ul>
             </div>
-        </form>
-    </div>
-</div>
 
-</div>
-</div>
-<div class="f-bottom col-xs-12">
-    <div class="container">
-        <p>@lang('general.copy_right')</p>
+            <div class="f-item col-md-3 col-sm-3 col-xs-12">
+                <h4>social</h4>
+                <div class="social-f">
+                    <a href="{{\App\Models\AppSetting::first()->facebook}}">
+                        <i class="fa fa-facebook"></i>
+                    </a>
+                    <a href="{{\App\Models\AppSetting::first()->twitter}}">
+                        <i class="fa fa-twitter"></i>
+                    </a>
+                    <a href="{{\App\Models\AppSetting::first()->linked_in}}">
+                        <i class="fa fa-linkedin"></i>
+                    </a>
+                    <a href="{{\App\Models\AppSetting::first()->instagram}}">
+                        <i class="fa fa-instagram"></i>
+                    </a>
+                    <a href="{{\App\Models\AppSetting::first()->whatsapp}}">
+                        <i class="fa fa-whatsapp"></i>
+                    </a>
+                </div>
+                <div class="f-form">
+                    <h4>@lang('general.newsletter')</h4>
+                    <p>@lang('general.newsletter_benefits')</p>
+                    <form action="{{route('add_subscriber')}}" method="POST">
+                        @csrf
+                        <div class="form-group">
+                            <input type="email" name="email" required class="form-control" placeholder="your email">
+                            <button class="footer-btn" type="submit"><i class="fa fa-paper-plane-o"></i></button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+        </div>
     </div>
-</div>
+    <div class="f-bottom col-xs-12">
+        <div class="container">
+            <p>@lang('general.copy_right')</p>
+        </div>
+    </div>
 </footer>
 <div class="toTop">
     <i class="fa fa-chevron-up"></i>
-</div> --}}
+</div>
