9bdc7f3e682271d8203feabacfb9f82f0fba958b
storeseg <84464012+storeseg@users.noreply.github.com>
2021-07-05 12:37:12 +0200

    asc

--- resources/views/Suppliers/show.blade.php
+++ resources/views/Suppliers/show.blade.php
@@ -1914,21 +1914,6 @@ Toast.fire({
 
 
 
-<script src="{{ asset('plugins/jquery.min.js')}}"></script>
-<script src="{{ asset('plugins/nouislider/nouislider.min.js')}}"></script>
-<script src="{{ asset('plugins/popper.min.js')}}"></script>
-<script src="{{ asset('plugins/owl-carousel/owl.carousel.min.js')}}"></script>
-<script src="{{ asset('plugins/bootstrap/js/bootstrap.min.js')}}"></script>
-<script src="{{ asset('plugins/imagesloaded.pkgd.min.js')}}"></script>
-<script src="{{ asset('plugins/masonry.pkgd.min.js')}}"></script>
-<script src="{{ asset('plugins/isotope.pkgd.min.js')}}"></script>
-<script src="{{ asset('plugins/jquery.matchHeight-min.js')}}"></script>
-<script src="{{ asset('plugins/slick/slick/slick.min.js')}}"></script>
-<script src="{{ asset('plugins/jquery-bar-rating/dist/jquery.barrating.min.js')}}"></script>
-<script src="{{ asset('plugins/slick-animation.min.js')}}"></script>
-<script src="{{ asset('plugins/lightGallery-master/dist/js/lightgallery-all.min.js')}}"></script>
-<script src="{{ asset('plugins/sticky-sidebar/dist/sticky-sidebar.min.js')}}"></script>
-<script src="{{ asset('plugins/gmap3.min.js')}}"></script>
 <!-- custom scripts-->
 <script src="{{ asset('plugins/supplier.js')}}"></script>
 @endpush
