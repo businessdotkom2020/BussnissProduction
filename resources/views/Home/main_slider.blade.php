28054dec57b52e2680eb8f049d35ed7d45877e97
storeseg <84464012+storeseg@users.noreply.github.com>
2021-07-06 17:46:06 +0200

    f

--- resources/views/Home/main_slider.blade.php
+++ resources/views/Home/main_slider.blade.php
@@ -1,90 +1,39 @@
-<div class="owl-carousel owl-theme owl-nav-inner owl-nav-md animation-slider owl-loaded owl-drag"
-data-owl-options="{
+<div class="owl-carousel owl-theme owl-nav-inner owl-nav-md animation-slider owl-loaded owl-drag" data-owl-options="{
 'nav': true,
 'dots': true
 }">
 
-<!-- End Of Intro Slide 1 -->
+    <!-- End Of Intro Slide 1 -->
 
-<!-- End of Intro Slide 3 -->
+    <!-- End of Intro Slide 3 -->
 
-<div class="owl-stage-outer">
-    <div class="owl-stage"
-        style="transform: translate3d(-832px, 0px, 0px); transition: all 0s ease 0s; width: 2496px;">
-        <div class="owl-item" style="width: 832px;">
-            <div class="banner banner-fixed intro-slide intro-slide1 br-sm" style="background-image: url(https://www.portotheme.com/html/wolmart/assets/images/demos/demo3/slides/slide-1.jpg);
+    <div class="owl-stage-outer">
+        <div class="owl-stage"
+            style="transform: translate3d(-832px, 0px, 0px); transition: all 0s ease 0s; width: 2496px;">
+            <div class="owl-item" style="width: 832px;">
+                <div class="banner banner-fixed intro-slide intro-slide1 br-sm" style="background-image: url(https://www.portotheme.com/html/wolmart/assets/images/demos/demo3/slides/slide-1.jpg);
     background-color: #262729;" data-index="1">
-                <div class="banner-content y-50 x-50 w-100 text-center">
-                    <h5 class="banner-subtitle text-primary font-weight-normal text-capitalize font-secondary ls-25 slide-animate"
-                        data-animation-options="{'name': 'fadeInDownShorter', 'duration': '.8s'}"
-                        style="animation-duration: 0.8s; animation-delay: 0.2s;">Mega Sale
-                    </h5>
-                    <h3 class="banner-title text-white text-capitalize ls-25 lh-1 slide-animate"
-                        data-animation-options="{'name': 'fadeInRightShorter', 'duration': '.5s', 'delay': '.5s'}"
-                        style="animation-duration: 0.5s; animation-delay: 0.5s;">
-                        2021 Men's Lifestyle</h3>
-                    <p class="ls-25 slide-animate" data-animation-options="{
-            'name': 'fadeInLeftShorter', 'duration': '.5s', 'delay': '.5s'
-        }" style="animation-duration: 0.5s; animation-delay: 0.5s;">Get Free Shipping on all orders over
-                        $99.00</p>
-                    <a href="shop-banner-sidebar.html"
-                        class="btn btn-white btn-outline btn-rounded btn-icon-right slide-animate"
-                        data-animation-options="{'name': 'fadeInUpShorter', 'duration': '.5s', 'delay': '.5s'}"
-                        style="animation-duration: 0.5s; animation-delay: 0.5s;">
-                        Shop Now<i class="w-icon-long-arrow-right"></i>
-                    </a>
+
+
                 </div>
             </div>
-        </div>
-        <div class="owl-item active" style="width: 832px;">
-            <div class="banner banner-fixed intro-slide intro-slide2  br-sm" style="background-image: url(https://www.portotheme.com/html/wolmart/assets/images/demos/demo3/slides/slide-2.jpg);
+            <div class="owl-item active" style="width: 832px;">
+                <div class="banner banner-fixed intro-slide intro-slide2  br-sm" style="background-image: url(https://www.portotheme.com/html/wolmart/assets/images/demos/demo3/slides/slide-2.jpg);
     background-color: #DCE0DF;" data-index="2">
-                <div class="banner-content y-50">
-                    <h5 class="banner-subtitle text-primary font-secondary font-weight-normal text-capitalize mb-0 lh-1 ls-25 slide-animate fadeInDownShorter show-content"
-                        data-animation-options="{'name': 'fadeInDownShorter', 'duration': '.8s'}" style="">
-                        Best
-                        Seller</h5>
-                    <h3 class="banner-title text-capitalize lh-.8 slide-animate fadeInDownShorter show-content"
-                        data-animation-options="{'name': 'fadeInDownShorter', 'duration': '.8s', 'delay': '.4s'}"
-                        style="">
-                        Kitchen Electronic</h3>
-                    <p class="text-default font-weight-normal ls-25 slide-animate fadeInDownShorter show-content"
-                        data-animation-options="{
-            'name': 'fadeInDownShorter', 'duration': '.8s', 'delay': '.6s'
-        }" style="">Get Free Shipping on all orders over <strong class="text-primary">$99.00</strong>
-                    </p>
-                    <a href="shop-banner-sidebar.html"
-                        class="btn btn-white btn-rounded btn-icon-right slide-animate fadeInDownShorter show-content"
-                        data-animation-options="{'name': 'fadeInDownShorter', 'duration': '.8s', 'delay': '.8s'}"
-                        style="">Shop
-                        Now<i class="w-icon-long-arrow-right"></i></a>
+
+
                 </div>
             </div>
-        </div>
-        <div class="owl-item" style="width: 832px;">
-            <div class="banner banner-fixed intro-slide intro-slide3 br-sm" style="background-image: url(https://www.portotheme.com/html/wolmart/assets/images/demos/demo3/slides/slide-3.jpg);
+            <div class="owl-item" style="width: 832px;">
+                <div class="banner banner-fixed intro-slide intro-slide3 br-sm" style="background-image: url(https://www.portotheme.com/html/wolmart/assets/images/demos/demo3/slides/slide-3.jpg);
     background-color: #696A6E;" data-index="3">
-                <div class="banner-content y-50 text-right">
-                    <div class="slide-animate" data-animation-options="{
-            'name': 'fadeInUpShorter', 'duration': '.7s'
-        }" style="animation-duration: 0.7s; animation-delay: 0.2s;">
-                        <h5
-                            class="banner-subtitle text-primary font-secondary font-weight-normal ls-25 mb-0 lh-1">
-                            New Arrivals</h5>
-                        <h3 class="banner-title text-white text-capitalize lh-1">Sports Sneakers</h3>
-                        <p class="text-white ls-25">Get Free Shipping on all orders over
-                            <strong>$99.00</strong></p>
-                        <a href="shop-banner-sidebar.html"
-                            class="btn btn-white btn-outline btn-rounded btn-icon-right">
-                            Shop Now<i class="w-icon-long-arrow-right"></i></a>
-                    </div>
+
                 </div>
             </div>
         </div>
     </div>
-</div>
-<div class="owl-nav"><button type="button" role="presentation" class="owl-prev"><i
-            class="w-icon-angle-left"></i></button><button type="button" role="presentation"
-        class="owl-next"><i class="w-icon-angle-right"></i></button></div>
-<div class="owl-dots disabled"></div>
+    <div class="owl-nav"><button type="button" role="presentation" class="owl-prev"><i
+                class="w-icon-angle-left"></i></button><button type="button" role="presentation" class="owl-next"><i
+                class="w-icon-angle-right"></i></button></div>
+    <div class="owl-dots disabled"></div>
 </div>
