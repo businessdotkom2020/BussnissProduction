653c8498886d66d928acd65b958a394c3f3630fb
storeseg <84464012+storeseg@users.noreply.github.com>
2021-07-06 20:39:46 +0200

    a

--- resources/views/Home/main_slider.blade.php
+++ resources/views/Home/main_slider.blade.php
@@ -4,60 +4,17 @@
 }">
     <div class="banner banner-fixed intro-slide intro-slide1 br-sm"
         style="background-image: url(https://www.portotheme.com/html/wolmart/assets/images/demos/demo8/slides/slide-1.jpg); background-color: #E8EAEF;">
-        <div class="banner-content y-50 text-right">
-            <div class="slide-animate" data-animation-options="{
-            'name': 'fadeInUpShorter', 'duration': '1s'
-        }">
-                <h5 class="banner-subtitle text-uppercase font-weight-bold mb-2">Deals And
-                    Promotions</h5>
-                <h3 class="banner-title text-capitalize ls-25">
-                    <span class="text-primary">Introducing</span><br>
-                    Fashion Lifestyle<br>Collection
-                </h3>
-                <a href="demo8-shop.html" class="btn btn-dark btn-outline btn-rounded btn-icon-right">
-                    Shop Now<i class="w-icon-long-arrow-right"></i>
-                </a>
-            </div>
-        </div>
+
     </div>
     <!-- End of Intro Slide 1 -->
     <div class="banner banner-fixed intro-slide intro-slide2 br-sm"
         style="background-image: url(https://www.portotheme.com/html/wolmart/assets/images/demos/demo8/slides/slide-2.jpg); background-color: #2e3233;">
-        <div class="banner-content y-50">
-            <div class="slide-animate" data-animation-options="{
-            'name': 'flipInY', 'duration': '1s'
-        }">
-                <h5 class="banner-subtitle text-white text-uppercase font-weight-bold">Smartphones
-                </h5>
-                <h3 class="banner-title text-white text-capitalize mb-0 ls-25">Sumsong Galaxy</h3>
-                <div class="banner-price-info text-white ls-25">Up to <strong class="text-secondary text-uppercase">30%
-                        Off</strong></div>
-                <p class="text-white">Get Free Shipping on all orders over <strong
-                        class="font-weight-bold">$199.99</strong></p>
-                <a href="demo8-shop.html" class="btn btn-white btn-outline btn-rounded btn-icon-right">
-                    Shop Now<i class="w-icon-long-arrow-right"></i>
-                </a>
-            </div>
-        </div>
+
     </div>
     <!-- End of Intro Slide 2 -->
     <div class="banner banner-fixed intro-slide intro-slide3 br-sm"
         style="background-image: url(https://www.portotheme.com/html/wolmart/assets/images/demos/demo8/slides/slide-3.jpg); background-color:#363332;">
-        <div class="banner-content y-50">
-            <div class="slide-animate" data-animation-options="{
-            'name': 'zoomIn', 'duration': '1s'
-        }">
-                <h5 class="banner-subtitle text-uppercase font-weight-normal text-secondary mb-2">
-                    From online store</h5>
-                <h3 class="banner-title text-white text-capitalize">Originals Comper<br>Star. Shoes
-                </h3>
-                <hr class="divider bg-white">
-                <p class="text-white text-uppercase mb-0 font-weight-bold">for - Women<br>
-                    <span class="font-weight-normal ls-normal">Product Identifier: </span><span
-                        class="text-secondary ls-normal">DD2098</span>
-                </p>
-            </div>
-        </div>
+
     </div>
     <!-- End of Intro Slide 3 -->
 </div>
