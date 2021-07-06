b31759e0162ec2c717c438dddea8f6aa41b9fe6c
storeseg <84464012+storeseg@users.noreply.github.com>
2021-07-06 17:51:52 +0200

    f

--- resources/views/Home/main_slider.blade.php
+++ resources/views/Home/main_slider.blade.php
@@ -1,33 +1,24 @@
 <div class="owl-carousel owl-theme owl-nav-inner owl-nav-md animation-slider owl-loaded owl-drag" data-owl-options="{
-'nav': true,
-'dots': true
-}">
-
+    'nav': true,
+    'dots': true
+    }">
     <!-- End Of Intro Slide 1 -->
-
     <!-- End of Intro Slide 3 -->
-
     <div class="owl-stage-outer">
-        <div class="owl-stage"
-            style="transform: translate3d(-832px, 0px, 0px); transition: all 0s ease 0s; width: 2496px;">
-            <div class="owl-item" style="width: 832px;">
+        <div class="owl-stage">
+            <div class="owl-item">
                 <div class="banner banner-fixed intro-slide intro-slide1 br-sm" style="background-image: url(https://www.portotheme.com/html/wolmart/assets/images/demos/demo3/slides/slide-1.jpg);
-    background-color: #262729;" data-index="1">
-
-
+                background-color: #262729;" data-index="1">
                 </div>
             </div>
-            <div class="owl-item active" style="width: 832px;">
+            <div class="owl-item active">
                 <div class="banner banner-fixed intro-slide intro-slide2  br-sm" style="background-image: url(https://www.portotheme.com/html/wolmart/assets/images/demos/demo3/slides/slide-2.jpg);
-    background-color: #DCE0DF;" data-index="2">
-
-
+                background-color: #DCE0DF;" data-index="2">
                 </div>
             </div>
-            <div class="owl-item" style="width: 832px;">
+            <div class="owl-item">
                 <div class="banner banner-fixed intro-slide intro-slide3 br-sm" style="background-image: url(https://www.portotheme.com/html/wolmart/assets/images/demos/demo3/slides/slide-3.jpg);
-    background-color: #696A6E;" data-index="3">
-
+                background-color: #696A6E;" data-index="3">
                 </div>
             </div>
         </div>
