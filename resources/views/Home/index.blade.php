e08bf87d2aaf8ad95f6e31b80403aa7ef4d71489
storeseg <84464012+storeseg@users.noreply.github.com>
2021-07-06 17:43:06 +0200

    f

--- resources/views/Home/index.blade.php
+++ resources/views/Home/index.blade.php
@@ -50,22 +50,8 @@ $menu = false ;
 
 <main class="main">
     <div class="intro-section">
-        <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img class="d-block w-100" src="https://eg.jumia.is/cms/eid-adha-21/sliders/g2.jpg"
-                        alt="First slide">
-                </div>
-                <div class="carousel-item">
-                    <img class="d-block w-100" src="https://eg.jumia.is/cms/eid-adha-21/sliders/g2.jpg"
-                        alt="Second slide">
-                </div>
-                <div class="carousel-item">
-                    <img class="d-block w-100" src="https://eg.jumia.is/cms/eid-adha-21/sliders/g2.jpg"
-                        alt="Third slide">
-                </div>
-            </div>
-        </div>
+        @include('home.main_slider')
+
     </div>
     <!-- End of .intro-section -->
 
