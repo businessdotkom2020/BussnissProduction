d5a660f35450f66a9d35916699e11d33b1bd7a6f
storeseg <84464012+storeseg@users.noreply.github.com>
2021-07-18 18:32:41 +0200

    f

---
+++ resources/views/Home/featuers_slider.blade.php
@@ -0,0 +1,38 @@
+<div class="owl-carousel owl-theme row cols-md-4 cols-sm-3 cols-1 icon-box-wrapper appear-animate br-sm mt-6 mb-10 appear-animate"
+    data-owl-options="{
+'nav': false,
+'dots': false,
+'loop': true,
+'autoplay': true,
+'autoplayTimeout': 4000,
+'responsive': {
+'0': {
+    'items': 1
+},
+'576': {
+    'items': 2
+},
+'768': {
+    'items': 3
+},
+'992': {
+    'items': 3
+},
+'1200': {
+    'items': 4
+}
+}
+}">
+    @foreach (\App\Models\Featuere::get() as $featuer_slider)
+
+    <div class="icon-box icon-box-side text-dark icon-box-chat">
+        <span class="icon-box-icon icon-chat">
+            <i class="{{$featuer_slider->icon}}"></i>
+        </span>
+        <div class="icon-box-content">
+            <h4 class="icon-box-title">{{$featuer_slider->title}}</h4>
+            <p class="text-default">{{$featuer_slider->body}}</p>
+        </div>
+    </div>
+</div>
+@endforeach
--- resources/views/Home/index.blade.php
+++ resources/views/Home/index.blade.php
@@ -66,69 +66,9 @@
 
             </div>
 
+            @include('Home.featuers_slider')
 
-            <div class="owl-carousel owl-theme row cols-md-4 cols-sm-3 cols-1 icon-box-wrapper appear-animate br-sm mt-6 mb-10 appear-animate"
-                data-owl-options="{
-            'nav': false,
-            'dots': false,
-            'loop': true,
-            'autoplay': true,
-            'autoplayTimeout': 4000,
-            'responsive': {
-                '0': {
-                    'items': 1
-                },
-                '576': {
-                    'items': 2
-                },
-                '768': {
-                    'items': 3
-                },
-                '992': {
-                    'items': 3
-                },
-                '1200': {
-                    'items': 4
-                }
-            }
-        }">
-                <div class="icon-box icon-box-side text-dark">
-                    <span class="icon-box-icon icon-shipping">
-                        <i class="w-icon-truck"></i>
-                    </span>
-                    <div class="icon-box-content">
-                        <h4 class="icon-box-title">Free Shipping & Returns</h4>
-                        <p class="text-default">For all orders over $99</p>
-                    </div>
-                </div>
-                <div class="icon-box icon-box-side text-dark">
-                    <span class="icon-box-icon icon-payment">
-                        <i class="w-icon-bag"></i>
-                    </span>
-                    <div class="icon-box-content">
-                        <h4 class="icon-box-title">Secure Payment</h4>
-                        <p class="text-default">We ensure secure payment</p>
-                    </div>
-                </div>
-                <div class="icon-box icon-box-side text-dark icon-box-money">
-                    <span class="icon-box-icon icon-money">
-                        <i class="w-icon-money"></i>
-                    </span>
-                    <div class="icon-box-content">
-                        <h4 class="icon-box-title">Money Back Guarantee</h4>
-                        <p class="text-default">Any back within 30 days</p>
-                    </div>
-                </div>
-                <div class="icon-box icon-box-side text-dark icon-box-chat">
-                    <span class="icon-box-icon icon-chat">
-                        <i class="w-icon-chat"></i>
-                    </span>
-                    <div class="icon-box-content">
-                        <h4 class="icon-box-title">Customer Support</h4>
-                        <p class="text-default">Call or email us 24/7</p>
-                    </div>
-                </div>
-            </div>
+
             <!-- End of Icon Box Wrapper -->
             <div class="row category-wrapper cols-lg-3 cols-sm-2 appear-animate mb-8 fadeIn appear-animation-visible"
                 style="animation-duration: 1.2s; ">
